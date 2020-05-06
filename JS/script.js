7a3d6960ee00b230859963c4c2ef65963b167f8d
tomMisson <11tmisson@gmail.com>
2020-05-06 22:46:14 +0100

    Added easter egg!

--- JS/script.js
+++ JS/script.js
@@ -4,4 +4,10 @@ const nextEdition = "3.0"
 $( document ).ready(() => {
     $('#year').html(nextYear)
     $('#edition').html(nextEdition)
+
+    $('#owl').click( function(){
+        console.log("Weeee!");
+        $(this).toggleClass('easter-egg');
+    
+    })
 })
